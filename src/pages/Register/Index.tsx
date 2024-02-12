9bf7748e1b0653ebf41d6b5ca9a8cf00e0a1a4c0
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-02-13 01:42:30 +0330

    create Login and register and forgetpassword and resetpassword

---
+++ src/pages/Register/Index.tsx
@@ -0,0 +1,10 @@
+
+function Register() {
+  return (
+    <>
+      <h1>Register</h1>
+    </>  
+  );
+}
+
+export default Register;
---
+++ src/pages/ResetPassword/Index.tsx
@@ -0,0 +1,11 @@
+
+function ResetPassword() {
+  return (
+    <>
+      <h1>Reset Password</h1>
+    </>
+    
+  );
+}
+
+export default ResetPassword;
