b71f078e16ec6d72661220e2ee5c1a705f165915
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-02-13 01:43:59 +0330

    define router in app.tsx and adjust with layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LayoutAuth from "./components/layout/auth";
+import Login from "./pages/Login/Index";
+import Register from "./pages/Register/Index";
+import ForgetPassword from "./pages/ForgetPassword/Index";
+import ResetPassword from "./pages/ResetPassword/Index";
 
-import './App.css';
-import LayoutAuth from './components/layout/auth';
 function App() {
   return (
-
-     <LayoutAuth>
-    <h1>your component</h1>
-     </LayoutAuth>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LayoutAuth />}> 
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="forgetpassword" element={<ForgetPassword />} />
+          <Route path="resetpassword" element={<ResetPassword />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
