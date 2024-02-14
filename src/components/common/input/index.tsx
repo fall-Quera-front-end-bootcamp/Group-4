a5153c23ad673a3047376ac9571274693000f851
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-02-14 23:03:37 +0330

    change stype as props

--- src/components/common/input/index.tsx
+++ src/components/common/input/index.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
 
 interface InputProps {
-    value: string | number; 
-    onChange: (value: string | number) => void;
-    width: number;
-    height: number;
-    placeholder?: string;
-  }
-  
-  const DynamicInput: React.FC<InputProps> = ({
-    value,
-    onChange,
-    width,
-    height,
-    placeholder,
-  }) => {
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      onChange(event.target.value);
-    };
+  value: string | number; 
+  onChange: (value: string | number) => void;
+  style?: React.CSSProperties; 
+  placeholder?: string;
+}
 
+const DynamicInput: React.FC<InputProps> = ({
+  value,
+  onChange,
+  style, 
+  placeholder,
+}) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
 
   return (
     <input
       type="text"
       className="border border-gray-300 rounded-md px-2 py-1 w-full"
       style={{
-        width: `${width}px`,
-        height: `${height}px`,
+        ...style, 
         borderRadius: '6px',
         border: '1px solid #e2e8f0',
       }}
