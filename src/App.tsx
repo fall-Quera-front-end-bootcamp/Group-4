4c21f3a9f28c6b242f7e46f58585518a2a644c35
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-02-18 00:36:48 +0330

    The first phase of the project was completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 
+
 const LayoutAuth = React.lazy(() => import("./app/Components/layout/auth"));
 const Login = React.lazy(() => import("./app/Authenticator/Components/Login"));
 const Register = React.lazy(
@@ -17,7 +18,6 @@ const ResetPassword = React.lazy(
 const NotFound = React.lazy(
   () => import("./app/Authenticator/Components/NotFound")
 );
-
 
 function App() {
   return (
