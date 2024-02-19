281be8332f5d949a33e669dc97f340096243b444
muhammadali-ghasemi <mm.ghasemi1378@gmail.com>
2024-02-19 16:51:01 +0330

    avatars component (for both singleavatar and multiavatar)

--- src/app/Components/layout/auth/index.tsx
+++ src/app/Components/layout/auth/index.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../../Navbar/index";
+import Navbar from "../../navbar/index";
 
 import "./style.css";
 interface Iprops extends React.PropsWithChildren {}
