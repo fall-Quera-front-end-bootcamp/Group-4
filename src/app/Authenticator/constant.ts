d479059a3cf6598fb1361fc05fe7f1f58a94c962
Sina Mohammadi <mohammadi.si@tiddev.com>
2024-02-19 12:30:00 +0330

    main store and reducer files, define slice

---
+++ src/app/Authenticator/constant.ts
@@ -0,0 +1,1 @@
+export const  FORGET_PASSWORD = "app/Authenticator/FORGET_PASSWORD"
--- src/app/Authenticator/slice.ts
+++ src/app/Authenticator/slice.ts
@@ -1,1 +1,26 @@
-export {}
+import { createSlice } from "@reduxjs/toolkit";
+import { FORGET_PASSWORD } from "./constant";
+
+export const initialState = {
+  mode: FORGET_PASSWORD,
+  dataList: {
+    taskItems: [],
+    tasks: [],
+    totalTasks: null,
+    init: {},
+  },
+};
+
+const slice = createSlice({
+  name: "App/Authenticator",
+  initialState,
+  reducers: {
+    setMode: (state, action) => {
+      state.mode = action.payload;
+    },
+  },
+});
+
+export const { setMode } = slice.actions;
+
+export default slice.reducer;
