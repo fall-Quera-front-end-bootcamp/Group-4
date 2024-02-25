6583406b928c1b6522c8464d028807f1dfa3ef96
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-02-25 13:58:32 +0330

    feat:added listView page

---
+++ src/app/Dashboard/HomePage/Board/ListView.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import List from '../../../Components/List'
+
+function ListView() {
+  return (
+    <div>
+        <List/>
+    </div>
+  )
+}
+
+export default ListView
