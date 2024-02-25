105309c07212183dd5e489df85e18b966c96ce8d
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-02-25 11:38:48 +0330

    fix: fix size of header and match with sidbar

--- src/app/Dashboard/HomePage/Board/Board.tsx
+++ src/app/Dashboard/HomePage/Board/Board.tsx
@@ -1,26 +1,14 @@
 import Header from "./Board-Component/Header-Board/Header";
-import ColumnOpen from "./Board-Component/Board-Column/Column-Open/Board-Column";
-import ColumnCard from "./Board-Component/Board-Column/Column-Card/ColumnCard"
-import ColumnCardImage from "./Board-Component/Board-Column/Column-Card-Image/ColumnCardImage";
-import ColumnButton from "./Board-Component/Board-Column/Column-Button/Column-Button";
-import WorkspaceModal from "./Board-Component/WorkspaceModal";
+import SideBar from "../../../Components/SideBar";
 
 function Board () {
     return (
-        <>
+        <div className="flex justify-center">
+            
             <Header/>
-            <div  className="w-[1038px] h-[804px] gap-[16px] flex-row flex">
-            <div  className=" flex fle flex-col w-[250px] h-[618px] gap-[16px] mt-4 ">
-                <ColumnOpen/>
-                <ColumnCardImage/>
-                <ColumnCard/>
-                <ColumnButton/>
-                <WorkspaceModal/>
-
-
-            </div>
-            </div>
-        </>
+            <SideBar/>
+           
+        </div>
     )
 }
 
