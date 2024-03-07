14c9939f433593f8a5ef1b620b8d266d0b73ed80
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-03-07 23:41:30 +0330

    fix: add MoreColumn modal to ... button in BoardColumn component

--- src/app/Dashboard/HomePage/Board/Board-Component/Board-Column/Column-Open/Board-Column.tsx
+++ src/app/Dashboard/HomePage/Board/Board-Component/Board-Column/Column-Open/Board-Column.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import MoreColumn from '../../../../../../Components/MoreColumn';
+import { useState } from 'react';
 
 interface ColumnOpenProps {
     state: string;
@@ -7,6 +9,12 @@ interface ColumnOpenProps {
 }
 
 const ColumnOpen: React.FC<ColumnOpenProps> = ({ state, number, borderColor }) => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
 
 
     return (
@@ -18,9 +26,10 @@ const ColumnOpen: React.FC<ColumnOpenProps> = ({ state, number, borderColor }) =
                 <span className="font-medium text-[12px] leading-[16.91px] text-right text-[#1E1E1E]">{state}</span>
             </div>
             <div className="flex flex-row justify-center h-[24px] gap-[4px]">
-                <span className="w-[24px] h-[24px]">+</span>
-                <span className="w-[20px] h-[20px] mb-4">...</span>
+                <span className="w-[24px] h-[24px] cursor-pointer">+</span>
+                <span className="w-[20px] h-[20px] mb-4 cursor-pointer" onClick={toggleModal}>...</span>
             </div>
+            {isModalOpen && <MoreColumn />}
         </div>
     );
 }
