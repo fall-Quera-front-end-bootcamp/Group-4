2265847f18a434d35ab26637146946f201705bb1
muhammadali-ghasemi <mm.ghasemi1378@gmail.com>
2024-03-07 10:58:47 +0330

    feat: tailwind color config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,20 @@ module.exports = {
     extend: {
       colors: {
         customBlue: 'rgba(32, 141, 142, 1)',
-        customBlueLight: 'rgba(15, 100, 100, 1)'
+        customBlueLight: 'rgba(15, 100, 100, 1)',
+        cornflowerBlue: '#4C6EF5',
+        ceruleanBlue: '#228BE6',
+        skyBlue: '#15AABF',
+        teal: '#12B886',
+        darkCyan: '#208D8E',
+        limeGreen: '#40C057',
+        electricLime: '#82C91E',
+        goldenYellow: '#FAB005',
+        orange: '#FD7E14',
+        redOrange: '#FA5252',
+        hotPink: '#E64980',
+        purple: '#BE4BDB',
+        blueViolet: '#7950F2',
       }
     },
   },
