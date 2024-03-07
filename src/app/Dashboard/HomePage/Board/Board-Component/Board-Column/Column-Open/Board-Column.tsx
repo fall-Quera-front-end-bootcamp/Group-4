a3dca4c4f4704d05b9d3e4d2786055d7843f4477
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-03-07 23:51:35 +0330

    fix: scroll in boardColumn component

--- src/app/Dashboard/HomePage/Board/Board-Component/Board-Column/Column-Open/Board-Column.tsx
+++ src/app/Dashboard/HomePage/Board/Board-Component/Board-Column/Column-Open/Board-Column.tsx
@@ -18,7 +18,7 @@ const ColumnOpen: React.FC<ColumnOpenProps> = ({ state, number, borderColor }) =
 
 
     return (
-        <div className={`flex-row-reverse flex justify-between h-[40px] gap-[80px] rounded-[16px] border-t-[2px] shadow border-[#FD7E14] bg-white pt-[8px] pr-[12px] pb-[8px] pl-[12px] overflow-x-auto`}>
+        <div className={`flex-row-reverse flex justify-between h-[40px] gap-[80px] rounded-[16px] border-t-[2px] shadow border-[#FD7E14] bg-white pt-[8px] pr-[12px] pb-[5px] pl-[12px] overflow-x-auto`}>
             <div className="h-[24px] gap-[4px] items-center justify-center flex flex-row">
                 <div className="bg-[#F4F4F4] rounded-[50px] w-[14px] h-[19px] flex justify-center items-center gap-[10px] pt-[2px] pr-[4px] pl-[4px]">
                     <span className="text-[#1E1E1E] w-[6px] h-[17px] font-medium text-[12px] leading-[16.91px] text-right">{number}</span>
