6178574e44d7e436edd2ada0935de8727cc6e139
muhammadali-ghasemi <mm.ghasemi1378@gmail.com>
2024-03-09 16:08:50 -0300

    fix: add project button style

--- src/app/Dashboard/HomePage/WorkspaceContainer/Workspace.tsx
+++ src/app/Dashboard/HomePage/WorkspaceContainer/Workspace.tsx
@@ -39,22 +39,24 @@ const Workspace: React.FC<WorkspaceProps> = ({ id, name, color }) => {
 
 
   return (
-    <div dir='rtl' className="flex flex-column gap-2 mb-6 mr-6 ">
+    <div dir='rtl' className="flex flex-col gap-2 mb-6 mr-6 ">
         <h1>{name}</h1>
-        <button className="w-[200px] h-[80px] border-2 border-red-500 bg-white text-red-500 py-2 px-4 rounded-lg hover:bg-red-500 hover:text-white flex justify-center items-center">
-          <h1 style={CreateNewProjectButtonStyle}>ساختن پروژه جدید</h1>
-          <img src={addNewProjectIcon} alt="addNewProjectIcon" />
-        </button>
-        <div className="flex flex-row gap-4">
-            {projects.map(project => (
-                <ProjectCard
-                    key={project.id}
-                    id={project.id}
-                    name={project.name}
-                    color={color}
-                    />
-            ))}
-        </div>
+          <div className='flex flex-row gap-4'>
+            <button className="w-[200px] h-[80px] border-2 border-red-500 bg-white text-red-500 py-2 px-4 rounded-lg hover:bg-red-500 hover:text-white flex justify-center items-center">
+              <img src={addNewProjectIcon} alt="addNewProjectIcon" />
+              <h1 style={CreateNewProjectButtonStyle}>ساختن پروژه جدید</h1>
+            </button>
+            <div className="flex flex-row gap-4">
+                {projects.map(project => (
+                    <ProjectCard
+                        key={project.id}
+                        id={project.id}
+                        name={project.name}
+                        color={color}
+                        />
+                ))}
+            </div>
+          </div>
         <div className='w-full h-[2px] bg-gray-200 mt-3' ></div>
     </div>
   );
