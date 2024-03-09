65c94ede1016ba3e98b385f58cdefea23651ff07
muhammadali-ghasemi <mm.ghasemi1378@gmail.com>
2024-03-09 19:10:26 -0300

    fix: new project button logic

--- src/app/Components/NewProjectModal/index.tsx
+++ src/app/Components/NewProjectModal/index.tsx
@@ -1,27 +1,22 @@
-// NewProjectModal.tsx
-
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 import DeleteIcon from '../../Components/assets/icons/Delete.png';
 import DynamicInput from '../common/input';
 import DynamicButton from '../common/button';
+import { createProject } from '../../../services/project';
 
 interface NewProjectModalProps {
   onCloseModal: () => void;
+  workspaceId: number; 
 }
 
-const NewProjectModal: React.FC<NewProjectModalProps> = ({ onCloseModal }) => {
+const NewProjectModal: React.FC<NewProjectModalProps> = ({ onCloseModal, workspaceId }) => {
   const [isOpen, setIsOpen] = useState(true);
-  const [workspaceName, setWorkspaceName] = useState<string>('');
-
+  const [projectName, setProjectName] = useState<string>('');
 
   const HeaderStyles = {
     fontWeight:800,
     fontSize:"24px"
-  };
-
-  const handleNext = () => {
-    console.log("hello")
   };
 
   const closeModal = () => {
@@ -29,55 +24,52 @@ const NewProjectModal: React.FC<NewProjectModalProps> = ({ onCloseModal }) => {
     onCloseModal();
   };
 
-  const handleWorkspaceNameChange = (newValue: string) => {
-    setWorkspaceName(newValue);
-    console.log(newValue);
+  const handleNext = async () => {
+    try {
+      await createProject(workspaceId, projectName);
+      closeModal(); 
+    } catch (error) {
+      console.error('Error creating project:', error);
+    }
+  };
+
+  const handleProjectNameChange = (newValue: string) => {
+    setProjectName(newValue);
   };
 
   const NameOfWorkSpaceStyle = {
     fontWeight:400,
     fontSize:"14px"
   };
+
   return (
     <div>
-        <Modal isOpen={isOpen} onRequestClose={closeModal}
-        style={{
-            content: {
-              width:'500px',
-              height:'268px',
-              top: '50%', 
-              left: '50%', 
-              transform: 'translate(-50%, -50%)',
-              borderRadius: '8px'
-            }
-          }}
-        >
-            <div className='flex w-[452px] h-[32px] justify-end items-center'>
-              <h3 className='pr-[105px]' style={HeaderStyles}>ساختن پروژه جدید‌</h3>
-              <img src={DeleteIcon} alt="DeleteIcon" className='w-[24px] h-[24px] cursor-pointer' onClick={closeModal}/>
-            </div>
-            <div className='w-[415px] h-[68px] flex flex-col justify-center items-end pt-[70px] ml-[20px]'>
-                      <label htmlFor="workspaceName" className='pb-[10px]' style={NameOfWorkSpaceStyle}>نام پروژه</label>
-                      <DynamicInput
-                      type='text'
-                      width={415}
-                      height={40}
-                      value={workspaceName}
-                      onChange={(newValue) => handleWorkspaceNameChange(newValue)}
-                      />
-            </div>
-            <DynamicButton
-                      text='ادامه'
-                      width={415}
-                      height={40}
-                      borderRadius={6}
-                      fontWeight={800}
-                      fontSize='14px'
-                      onClick={handleNext}
-                      style={{marginLeft:"20px" , marginTop:"80px"}}
-                   />
-
-        </Modal>
+      <Modal isOpen={isOpen} onRequestClose={closeModal} style={{ content: { width:'500px', height:'268px', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', borderRadius: '8px' } }}>
+        <div className='flex w-[452px] h-[32px] justify-end items-center'>
+          <h3 className='pr-[105px]' style={HeaderStyles}>ساختن پروژه جدید‌</h3>
+          <img src={DeleteIcon} alt="DeleteIcon" className='w-[24px] h-[24px] cursor-pointer' onClick={closeModal}/>
+        </div>
+        <div className='w-[415px] h-[68px] flex flex-col justify-center items-end pt-[70px] ml-[20px]'>
+          <label htmlFor="projectName" className='pb-[10px]' style={NameOfWorkSpaceStyle}>نام پروژه</label>
+          <DynamicInput
+            type='text'
+            width={415}
+            height={40}
+            value={projectName}
+            onChange={(newValue) => handleProjectNameChange(newValue)}
+          />
+        </div>
+        <DynamicButton
+          text='ادامه'
+          width={415}
+          height={40}
+          borderRadius={6}
+          fontWeight={800}
+          fontSize='14px'
+          onClick={handleNext}
+          style={{marginLeft:"20px" , marginTop:"80px"}}
+        />
+      </Modal>
     </div>
   )
 }
