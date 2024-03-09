717c2f835786ccf2845014757ecb368a7456defa
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-03-10 02:52:58 +0330

    feat:create profile1 page

---
+++ src/app/Profile/ProfilePages/Profile2.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function profile2() {
+  return (
+    <div className='w-100%'>profile2</div>
+  )
+}
+
+export default profile2
---
+++ src/app/Profile/ProfilePages/Profile3.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function profile3() {
+  return (
+    <div className='w-100%'>profile3</div>
+  )
+}
+
+export default profile3
