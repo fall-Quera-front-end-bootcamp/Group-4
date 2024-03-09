12c09dcd0447536193748decb6aa1fae406bb637
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-03-10 01:11:24 +0330

    fix: connect newProjectModal to button in workspace page

--- src/app/Dashboard/HomePage/Board/ListView.tsx
+++ src/app/Dashboard/HomePage/Board/ListView.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import List from '../../../Components/List'
-import NewProjectModal from '../../../Components/NewProjectModal'
 
 function ListView() {
   return (
     <div>
         <List/>
-        <NewProjectModal/>
     </div>
   )
 }
