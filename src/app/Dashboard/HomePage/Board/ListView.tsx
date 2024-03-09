3440ec2a954c54244b211dfdada5d323ead92e10
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-03-09 23:05:09 +0330

    feat: add New project Modal

--- src/app/Dashboard/HomePage/Board/ListView.tsx
+++ src/app/Dashboard/HomePage/Board/ListView.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import List from '../../../Components/List'
+import NewProjectModal from '../../../Components/NewProjectModal'
 
 function ListView() {
   return (
     <div>
         <List/>
+        <NewProjectModal/>
     </div>
   )
 }
