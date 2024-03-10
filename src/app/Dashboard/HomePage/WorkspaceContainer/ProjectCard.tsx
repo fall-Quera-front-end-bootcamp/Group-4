ed12bc1dfd356a2eb70d1c27656710a6333b7ce7
muhammadali-ghasemi <mm.ghasemi1378@gmail.com>
2024-03-10 16:49:02 -0300

    feat:projects onclick id set

--- src/app/Dashboard/HomePage/WorkspaceContainer/ProjectCard.tsx
+++ src/app/Dashboard/HomePage/WorkspaceContainer/ProjectCard.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useDispatch } from "react-redux";
+import { setWorkspaceId } from "../../../../Features/workspaceSlice"
+import { setProjectId } from "../../../../Features/projectSlice"
 
 interface ProjectCardProps {
+  workspaceId: number;
   id: number;
   name: string;
   color: string;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ id, name, color }) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({ workspaceId,id, name, color}) => {
+  const dispatch = useDispatch();
+
+  const handleProjectClick: React.MouseEventHandler<HTMLDivElement> = (event) => {
+    dispatch(setWorkspaceId(workspaceId));
+    dispatch(setProjectId(id)); 
+    console.log("project ID:", id);
+    console.log("workspace ID:", workspaceId);
+  };
+
+
+
   return (
     <>
     {/* <div className= 'flex justify-center items-center bg-gradient-to-br from-cornflowerBlue to-cornflowerBlueLight bg-opacity-50 p-4 rounded-lg w-[200px] h-[80px] cursor cursor-pointer'></div>
@@ -22,7 +37,9 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ id, name, color }) => {
       <div className= 'flex justify-center items-center bg-gradient-to-br from-hotPink to-hotPinkLight bg-opacity-50 p-4 rounded-lg w-[200px] h-[80px] cursor cursor-pointer'></div>
       <div className= 'flex justify-center items-center bg-gradient-to-br from-purple to-purpleLight bg-opacity-50 p-4 rounded-lg w-[200px] h-[80px] cursor cursor-pointer'></div>
       <div className= 'flex justify-center items-center bg-gradient-to-br from-blueViolet to-blueVioletLight bg-opacity-50 p-4 rounded-lg w-[200px] h-[80px] cursor cursor-pointer'></div> */}
-    <div className={`flex justify-center items-center bg-gradient-to-br from-${color} to-${color}Light bg-opacity-50 p-4 rounded-lg w-[200px] h-[80px] cursor cursor-pointer`}>
+    <div className={`flex justify-center items-center bg-gradient-to-br from-${color} to-${color}Light bg-opacity-50 p-4 rounded-lg w-[200px] h-[80px] cursor cursor-pointer`}
+         onClick={handleProjectClick}
+    >
       <p className="text-xl text-white font-bold mt-3">{name}</p>
 
     </div>
