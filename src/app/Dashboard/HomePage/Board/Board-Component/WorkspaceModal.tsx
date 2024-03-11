1c14bde0cccb6ad4e4ef96ae765fb56a422be2cb
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-03-11 04:55:00 +0330

    fix: delete previous steper modal

--- src/app/Dashboard/HomePage/Board/Board-Component/WorkspaceModal.tsx
+++ src/app/Dashboard/HomePage/Board/Board-Component/WorkspaceModal.tsx
@@ -1,7 +1,4 @@
-import WorkspaceOne from "./Workspace-Slides-Group-3/Workspace-One";
 import React from "react";
-import WorkspaceTwo from "./Workspace-Slides-Group-3/Workspace-Two";
-import WorkspaceThree from "./Workspace-Slides-Group-3/Workspace-Three";
 import {Link, Route, Routes} from "react-router-dom";
 import DynamicButton from "../../../../Components/common/button";
 import ShareWorkspace from "./Board-Column/Column-More/New-Project/Share-Workspace/ShareWorkspace";
