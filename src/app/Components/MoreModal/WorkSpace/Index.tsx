7cdd59603a8fc6354ee974eddea71ffcbe21da11
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-03-12 23:21:32 +0330

    fix: connect Share work space modal to button in modal

--- src/app/Components/MoreModal/WorkSpace/Index.tsx
+++ src/app/Components/MoreModal/WorkSpace/Index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import AddIcon from '../../assets/icons/MoreColumn/add.png';
 import EditNameIcon from '../../assets/icons/MoreColumn/editColumn.png';
 import EditColorIcon from '../../assets/icons/MoreColumn/editColor.png';
 import CopyLinkIcon from '../../assets/icons/MoreColumn/CopyLink.png';
-import DeleteIcon from '../../assets/icons/MoreColumn/deleteRedIcon.png'
+import DeleteIcon from '../../assets/icons/MoreColumn/deleteRedIcon.png';
 import DynamicButton from '../../common/button';
-import ShareIcon from '../../assets/icons/MoreColumn/ShareIcon.png'
+import ShareIcon from '../../assets/icons/MoreColumn/ShareIcon.png';
+import ShareWorkspace from '../../ShareWorkSpace';
 
 const customStyles = {
     content: {
@@ -31,6 +32,16 @@ const ItemsStyle = {
 }
 
 const MoreWorkSpaceModal: React.FC<{ closeModal: () => void }> = ({ closeModal }) => {
+    const [shareModalOpen, setShareModalOpen] = useState(false);
+
+    const openShareModal = () => {
+        setShareModalOpen(true);
+    };
+
+    const closeShareModal = () => {
+        setShareModalOpen(false);
+    };
+
     return (
         <Modal
             isOpen={true}
@@ -63,12 +74,33 @@ const MoreWorkSpaceModal: React.FC<{ closeModal: () => void }> = ({ closeModal }
                 text='اشتراک گذاری'
                 width={169}
                 height={36}
-                onClick={() => console.log("hojjat")}
+                onClick={openShareModal}
                 borderRadius={6}
                 fontSize="12px"
                 fontWeight={800}
                 icon={ShareIcon}
             />
+            <Modal
+                isOpen={shareModalOpen}
+                onRequestClose={closeShareModal}
+                style={{
+                    content: {
+                        width: '550px',
+                        height: '400px',
+                        borderRadius: '8px',
+                        border: 'none',
+                        padding: '20px',
+                        top: '50%',
+                        left: '50%',
+                        right: 'auto',
+                        bottom: 'auto',
+                        marginRight: '-50%',
+                        transform: 'translate(-50%, -50%)',
+                    }
+                }}
+            >
+                <ShareWorkspace  closeModal={closeModal}/>
+            </Modal>
         </Modal>
     );
 };
