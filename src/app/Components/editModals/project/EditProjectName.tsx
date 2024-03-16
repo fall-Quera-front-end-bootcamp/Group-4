04baa0d97f0fe2199eeb04a421e228e1a34b5503
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-03-17 02:29:52 +0330

    fix: style of edite project modal

--- src/app/Components/editModals/project/EditProjectName.tsx
+++ src/app/Components/editModals/project/EditProjectName.tsx
@@ -70,7 +70,7 @@ function EditProjectName({ onCloseModal }: EditProjectNameProps) {
       >
       <div className='w-[452px] h-[140px]'>
       <div className='flex w-[452px] h-[32px] justify-end items-center'>
-              <h3 className='pr-[85px]' style={HeaderStyles}>ویرایش نام پروژه</h3>
+              <h3 className='pr-[110px]' style={HeaderStyles}>ویرایش نام پروژه</h3>
               <img src={DeleteIcon} alt="DeleteIcon" className='w-[24px] h-[24px] cursor-pointer' onClick={onCloseModal}/>
             </div>
            <div className='flex flex-col h-[190px] justify-between'>
