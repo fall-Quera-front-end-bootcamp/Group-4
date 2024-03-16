0f405dbd6f3dc4db4de1be40e9353b1a48992984
HojjatIranmanesh <iranmanesh_hojat@yahoo.com>
2024-03-16 20:23:46 +0330

    fix: style of delete modal

--- src/app/Components/editModals/workspace/DeleteWorkspace.tsx
+++ src/app/Components/editModals/workspace/DeleteWorkspace.tsx
@@ -59,13 +59,13 @@ function DeleteWorkspace({ onCloseModal }: DeleteWorkspaceProps) {
         }}
       >
       <div className='w-[452px] h-[140px]'>
-      <div className='flex w-[452px] h-[32px] justify-end items-center'>
-              <h3 className='pr-[85px]' style={HeaderStyles}>آیا مایل به حذف ورکسپیس هستید ؟</h3>
+          <div className='flex w-[452px] h-[32px] justify-end items-center'>
+              
               <img src={DeleteIcon} alt="DeleteIcon" className='w-[24px] h-[24px] cursor-pointer' onClick={onCloseModal}/>
-            </div>
-           <div className='flex flex-col h-[190px] justify-between'>
-                  <div className='w-[415px] h-[68px] flex flex-col justify-center items-end pt-[70px] ml-[20px]'>
-
+          </div>
+          <div className='flex flex-col h-[190px] justify-between'>
+                  <div className='w-[415px] h-[68px] flex flex-col justify-center items-center pt-[70px] ml-[20px]'>
+                  <h3 className=' pb-[10px]' style={HeaderStyles}>آیا مایل به حذف ورکسپیس هستید ؟</h3>
                   </div>
                   <DynamicButton
                       text='بله'
